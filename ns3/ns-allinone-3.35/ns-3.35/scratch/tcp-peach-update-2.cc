cc013201495e4347e7fe92b1da86ab86f7a6ed7c
asifajrof <1705092@ugrad.cse.buet.ac.bd>
2022-02-06 20:37:17 +0600

    before loop for flow

--- ns3/ns-allinone-3.35/ns-3.35/scratch/tcp-peach-update-2.cc
+++ ns3/ns-allinone-3.35/ns-3.35/scratch/tcp-peach-update-2.cc
@@ -1,5 +1,24 @@
+/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
 /*
- * This is a simple example to test TCP over 802.11
+ * Copyright (c) 2015, IMDEA Networks Institute
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Hany Assasa <hany.assasa@gmail.com>
+ * Editor: Asif Ajrof <1705092@ugrad.cse.buet.ac.bd>
+.*
+ * This is a simple example to test TCP over 802.11n (with MPDU aggregation enabled).
  *
  * Network topology:
  *
@@ -14,16 +33,6 @@
  *                                           Wifi 10.1.2.0
  *
  */
-
-// #include "ns3/core-module.h"
-// #include "ns3/point-to-point-module.h"
-// #include "ns3/network-module.h"
-// #include "ns3/applications-module.h"
-// #include "ns3/mobility-module.h"
-// #include "ns3/csma-module.h"
-// #include "ns3/internet-module.h"
-// #include "ns3/yans-wifi-helper.h"
-// #include "ns3/ssid.h"
 
 #include "ns3/config.h"
 #include "ns3/string.h"
